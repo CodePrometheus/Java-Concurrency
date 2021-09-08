217db7a7d929b303bdd7e944cbd1d92a51ea7c9a
CodePrometheus <Code_Prince@outlook.com>
2021-09-08 23:10:50 +0800

    feat

---
+++ Interview/src/printParityExample01.java
@@ -0,0 +1,48 @@
+/**
+ * 打印奇偶数 1，2
+ * Synchronized + wait/notify
+ *
+ * @Author: zzStar
+ * @Date: 09-08-2021 22:42
+ */
+public class printParityExample01 {
+
+    public static void main(String[] args) {
+        print print = new print();
+        Thread a = new Thread(print, "A");
+        Thread b = new Thread(print, "B");
+        a.start();
+        b.start();
+    }
+
+}
+
+class print implements Runnable {
+
+    int i = 1;
+
+    /**
+     * 一个线程打印后唤醒另一个，再把自己挂起
+     */
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (this) {
+                // notify和wait只能用于synchronized内部
+                this.notify();
+                if (i < 100) {
+                    System.out.println(Thread.currentThread().getName() + " == " + i++);
+                } else {
+                    return;
+                }
+                try {
+                    // wait的前提是当前线程持有锁
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ Interview/src/printParityExample02.java
@@ -0,0 +1,77 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 打印奇偶数 1，2
+ * ReentrantLock
+ *
+ * @Author: zzStar
+ * @Date: 09-08-2021 22:53
+ */
+public class printParityExample02 {
+
+    private int start = 1;
+
+    private volatile boolean flag = false;
+
+    private final static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        printParityExample02 example02 = new printParityExample02();
+        Thread t1 = new Thread(new odd(example02), "A");
+        Thread t2 = new Thread(new even(example02), "B");
+        t1.start();
+        t2.start();
+    }
+
+    public static class odd implements Runnable {
+
+        private printParityExample02 number;
+
+        public odd(printParityExample02 number) {
+            this.number = number;
+        }
+
+        @Override
+        public void run() {
+            while (number.start < 100) {
+                if (!number.flag) {
+                    try {
+                        lock.lock();
+                        System.out.println(Thread.currentThread().getName() + "===" + number.start);
+                        number.start++;
+                        number.flag = true;
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class even implements Runnable {
+
+        private printParityExample02 number;
+
+        public even(printParityExample02 number) {
+            this.number = number;
+        }
+
+        @Override
+        public void run() {
+            while (number.start < 100) {
+                if (number.flag) {
+                    try {
+                        lock.lock();
+                        System.out.println(Thread.currentThread().getName() + "===" + number.start);
+                        number.start++;
+                        number.flag = false;
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+
+}
